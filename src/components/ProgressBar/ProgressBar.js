be78a71c9eb8bcb845033ea882ee3b0504322ab9
Chen Jie <e0036922@u.nus.edu>
2021-05-06 07:04:27 +0800

    Initial Commit

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,8 +5,26 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
-const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+const SIZES = {
+  small: {
+    '--height': 8 + 'px',
+    '--borderRadius': 4 + 'px',
+  },
+  medium: {
+    '--height': 12 + 'px',
+    '--borderRadius': 4 + 'px',
+  },
+  large: {
+    '--height': 24 + 'px',
+    '--borderRadius': 8 + 'px',
+  },
 };
+
+const ProgressBar = ({ value, size }) => {
+  const styles = SIZES[size];
+  return <Bar style={styles}>{value}</Bar>;
+};
+
+const Bar = styled.div``;
 
 export default ProgressBar;
